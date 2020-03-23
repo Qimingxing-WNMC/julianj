fd747c7f742f463a4336bada5cb246509cd79e6f
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2020-03-23 12:19:05 +0100

    Update compile.jl

--- compile.jl
+++ compile.jl
@@ -1,3 +1,3 @@
 using PackageCompiler
-create_sysimage([:DataFrames,:CSV, :CSVFiles, :DataFramesMeta, :GLM, :Plots, :StatsPlots, :GR, :StatsModels, :Distributions, :StatsFuns, :SpecialFunctions, :PlotUtils], replace_default = true, precompile_statements_file="precompile_statements.jl")
+create_sysimage([:DataFrames,:CSV, :CSVFiles, :DataFramesMeta, :GLM, :Plots, :StatsPlots, :GR, :StatsModels, :Distributions, :StatsFuns, :SpecialFunctions, :PlotUtils], replace_default = true, precompile_execution_file="precompile_statements.jl")
 
